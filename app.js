fb68d04388c1fe84add5955695024372d9c7b19b
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-10-23 09:56:33 -0400

    started javascript

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-*{
-  padding: 0;
-  margin: 0;
+const player1Score = 0;
+const player2Score = 0;
+
+const nike = document.getElementById('box1');
+const adidas = document.getElementById('box2');
+const reebok = document.getElementById('box3');
+const newBalance = document.getElementById('box4');
+const jordan = document.getElementById('box5');
+
+
+
+function gameWin() {
 
 }
