24347a0be686d6cbbe2d45bf2fec876843d6b31e
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-10-24 16:50:16 -0400

    added commentary text

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let player1Score = 0;
 let player2Score = 0;
+let gameOver;
 
 const nike = document.querySelector('#box1');
 const adidas = document.querySelector('#box2');
@@ -11,17 +12,16 @@ let scoreBox = document.querySelector('.scorebox');
 let score1 = document.querySelector('.player1Score');
 let score2 = document.querySelector('.player2Score');
 
-//win logic
-//nike beats adidas and Jordan
-//adidas beats new balance and Reebok
-//reebok beats nike and Jordan
-//new balance beats nike and Reebok
-//jordan beats new balance and adidas
+let commentary = document.querySelector('#commentary-text');
+let player1Text = document.querySelector('.p1text');
+let player2Text = document.querySelector('.p2text');
 
 function player2Choice() {
   let choices = ['nike', 'adidas', 'reebok', 'new balance', 'jordan'];
   let random = Math.floor(Math.random() * 5)
+  player2Text.innerHTML = 'Le Whack picked ' + choices[random];
   return choices[random];
+
 }
 
 function gameConditions (choice) {
@@ -29,76 +29,76 @@ function gameConditions (choice) {
   let player2 = player2Choice();
   console.log(`user2 choice: ${player2}`)
   if ( choice === 'nike' && (player2 === 'adidas' || player2 === 'jordan')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'adidas' && (player2 === 'new balance' || player2 === 'reebok')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'reebok' && (player2 === 'nike' || player2 === 'jordan')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'new balance' && (player2 === 'nike' || player2 === 'reebok')) {
-      console.log('BELIEVE THE HYPE')
       return win();
   } else if ( choice === 'jordan' && (player2 === 'new balance' || player2 === 'adidas')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( player2 === 'nike' && (choice === 'adidas' || choice === 'jordan')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'adidas' && (choice === 'new balance' || choice === 'reebok')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'reebok' && (choice === 'nike' || choice === 'jordan')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'new balance' && (choice === 'nike' || choice === 'reebok')) {
-      console.log('Take the L');
-      return lose();
   } else if ( player2 === 'jordan' && (choice === 'new balance' || choice === 'adidas')) {
-      console.log('Take the L');
       return lose();
   } else {
     return draw();
   }
-
 }
 
-function win() {
+function win(choice) {
   score1.innerHTML = player1Score++;
+  if(player1Score >= 10) {
+    alert('BELIEVE THE HYPE.')
+  }
 }
 
 function lose() {
   score2.innerHTML = player2Score++;
+  if(player1Score >= 10) {
+    alert('TAKE THIS L')
+}
+
+// function draw () {
+//
+// }
+
+function p1Message(choice) {
+  player1Text.innerHTML = `HYPEGAWD chose ${choice} |`
 }
 
 
-//tie logic
-// if both users choose same element
-//-- play again
-// if both users don't choose element with winning formula
-//-- do it again
-
-function draw () {
-  console.log(`it's a draw`);
-}
 
 function action() {
   nike.addEventListener('click', function(){
     gameConditions('nike');
+    p1Message('nike');
   });
+
   adidas.addEventListener('click', function(){
     gameConditions('adidas');
+    p1Message('adidas');
   });
+
   reebok.addEventListener('click', function(){
     gameConditions('reebok');
+    p1Message('reebok');
   });
+
   newBalance.addEventListener('click', function(){
     gameConditions('new balance');
+    p1Message('new balance');
   });
+
   jordan.addEventListener('click', function(){
     gameConditions('jordan');
+    p1Message('jordan');
   });
 }
-
 action();
