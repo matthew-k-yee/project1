b2e81baff55e6ee9b9701a63268067661daddda2
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-10-25 10:33:40 -0400

    fixed scoreboard bug

--- app.js
+++ app.js
@@ -29,50 +29,60 @@ function gameConditions (choice) {
   let player2 = player2Choice();
   console.log(`user2 choice: ${player2}`)
   if ( choice === 'nike' && (player2 === 'adidas' || player2 === 'jordan')) {
+      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'adidas' && (player2 === 'new balance' || player2 === 'reebok')) {
+      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'reebok' && (player2 === 'nike' || player2 === 'jordan')) {
+      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'new balance' && (player2 === 'nike' || player2 === 'reebok')) {
+      console.log('BELIEVE THE HYPE')
       return win();
   } else if ( choice === 'jordan' && (player2 === 'new balance' || player2 === 'adidas')) {
+      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( player2 === 'nike' && (choice === 'adidas' || choice === 'jordan')) {
+      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'adidas' && (choice === 'new balance' || choice === 'reebok')) {
+      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'reebok' && (choice === 'nike' || choice === 'jordan')) {
+      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'new balance' && (choice === 'nike' || choice === 'reebok')) {
+      console.log('Take the L');
   } else if ( player2 === 'jordan' && (choice === 'new balance' || choice === 'adidas')) {
+      console.log('Take the L');
       return lose();
   } else {
-    return draw();
+    player1Text.innerHTML = 'TIE'
   }
 }
 
 function win(choice) {
-  score1.innerHTML = player1Score++;
-  if(player1Score >= 10) {
-    alert('BELIEVE THE HYPE.')
+  player1Score++;
+  score1.innerHTML = player1Score;
+  console.log(`this is player1Score: ${player1Score}`);
+  if(player1Score > 21) {
+    alert('BELIEVE THE HYPE. YOU WIN!')
   }
 }
 
 function lose() {
-  score2.innerHTML = player2Score++;
-  if(player1Score >= 10) {
+  player2Score++;
+  score2.innerHTML = player2Score;
+  console.log(`this is player2Score: ${player2Score}`);
+  if(player2Score > 21) {
     alert('TAKE THIS L')
+  }
 }
-
-// function draw () {
-//
-// }
 
 function p1Message(choice) {
   player1Text.innerHTML = `HYPEGAWD chose ${choice} |`
 }
-
 
 
 function action() {
