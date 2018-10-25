13bc5d86845b3bfca3e953576a0ebac98ead38ef
Matthew Yee <Yeezus@ool-ad02029d.dyn.optonline.net>
2018-10-25 17:33:47 -0400

    added player1 images to gamezone

--- app.js
+++ app.js
@@ -12,16 +12,21 @@ let scoreBox = document.querySelector('.scorebox');
 let score1 = document.querySelector('.player1Score');
 let score2 = document.querySelector('.player2Score');
 
+let show_img1 =  document.querySelector('.player1gameZone')
+let show_img2 = document.querySelector('.player2gameZone')
+
 let commentary = document.querySelector('#commentary-text');
 let player1Text = document.querySelector('.p1text');
 let player2Text = document.querySelector('.p2text');
 
-const nike_img = document.querySelector('img#img1')
-const adidas_img = document.querySelector('img#img2')
-const asics_img = document.querySelector('img#img3')
-const newBalance_img = document.querySelector('img#img4')
-const jordan_img = document.querySelector('img#img5')
+const nike_box = document.querySelector('img#img1')
+const adidas_box = document.querySelector('img#img2')
+const asics_box = document.querySelector('img#img3')
+const newBalance_box = document.querySelector('img#img4')
+const jordan_box = document.querySelector('img#img5')
 
+let bigImg = '';
+const playerOneImg = document.querySelector('#player_1_img');
 
 function player2Choice() {
   let choices = ['nike', 'adidas', 'asics', 'new balance', 'jordan'];
@@ -35,33 +40,23 @@ function gameConditions (choice) {
   let player2 = player2Choice();
   console.log(`user2 choice: ${player2}`)
   if ( choice === 'nike' && (player2 === 'adidas' || player2 === 'jordan')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'adidas' && (player2 === 'new balance' || player2 === 'asics')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'asics' && (player2 === 'nike' || player2 === 'jordan')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( choice === 'new balance' && (player2 === 'nike' || player2 === 'asics')) {
-      console.log('BELIEVE THE HYPE')
       return win();
   } else if ( choice === 'jordan' && (player2 === 'new balance' || player2 === 'adidas')) {
-      console.log('BELIEVE THE HYPE!');
       return win();
   } else if ( player2 === 'nike' && (choice === 'adidas' || choice === 'jordan')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'adidas' && (choice === 'new balance' || choice === 'asics')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'asics' && (choice === 'nike' || choice === 'jordan')) {
-      console.log('Take the L!');
       return lose();
   } else if ( player2 === 'new balance' && (choice === 'nike' || choice === 'asics')) {
-      console.log('Take the L');
   } else if ( player2 === 'jordan' && (choice === 'new balance' || choice === 'adidas')) {
-      console.log('Take the L');
       return lose();
   } else {
     player1Text.innerHTML = 'TIE'
@@ -92,29 +87,48 @@ function p1Message(choice) {
 
 
 function action() {
-  nike.addEventListener('click', function(){
+  nike.addEventListener('click', function(e){
+    // console.log(e)
+    // console.log(e.target.src);
+    bigImg = e.target.src;
+    playerOneImg.src = bigImg;
     gameConditions('nike');
     p1Message('nike');
+
   });
 
-  adidas.addEventListener('click', function(){
+  adidas.addEventListener('click', function(e){
+    bigImg = e.target.src;
+    playerOneImg.src = bigImg;
     gameConditions('adidas');
     p1Message('adidas');
+
   });
 
-  asics.addEventListener('click', function(){
+  asics.addEventListener('click', function(e){
+    bigImg = e.target.src;
+    playerOneImg.src = bigImg;
     gameConditions('asics');
     p1Message('asics');
   });
 
-  newBalance.addEventListener('click', function(){
+  newBalance.addEventListener('click', function(e){
+    bigImg = e.target.src;
+    playerOneImg.src = bigImg;
     gameConditions('new balance');
     p1Message('new balance');
   });
 
-  jordan.addEventListener('click', function(){
+  jordan.addEventListener('click', function(e){
+    bigImg = e.target.src;
+    playerOneImg.src = bigImg;
     gameConditions('jordan');
     p1Message('jordan');
   });
 }
 action();
+
+
+function nike_img () {
+
+}
